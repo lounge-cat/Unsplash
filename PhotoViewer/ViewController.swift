59cb86a783da74cc3bfa1f323be75ca3d456a352
loungecat <loungecat0@gmail.com>
2022-11-04 17:48:00 -0700

    Create project

---
+++ PhotoViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoViewer
+//
+//  Created by  admin on 4.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
